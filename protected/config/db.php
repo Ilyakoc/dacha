f53d525ce9fea0729284d9b5b80ee2c9624a1611
Sergey <Lukichev_s@mail.ru>
2022-04-15 11:07:24 +0700

    site

---
+++ protected/config/db.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * DB connection config
+ */
+return array(
+    'connectionString' => 'mysql:host=localhost;dbname=cl30234_moto',
+    'username' => 'cl30234_moto',
+    'password' => 'n2hjna5T',
+);
